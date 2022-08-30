1db12c7b9224d92a669d7f9684fc76b3cda769e9
saiteja37 <saitejakandadi7@gmail.com>
2022-08-30 12:27:40 +0530

    we desrve best

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+df=pickle.load(open("book.pkl","rb"))
+pt=pickle.load(open("pt.pkl","rb"))
+sim=pickle.load(open("sim.pkl","rb"))
+book=pickle.load((open("books.pkl","rb")))
+@app.route('/')
+def index():
+    return  render_template("index.html",
+                            book_name=list(df['Book-Title'].values),
+                            author=list(df['Book-Author'].values),
+                            image=list(df['Image-URL-M'].values),
+                            votes=list(df['num_ratings'].values),
+                            rating=np.round(list(df['avg_ratings'].values),2)
+                            )
+@app.route('/rec')
+def recommend_ui():
+    return render_template('recomedation.html')
+@app.route("/recom",methods=['GET','POST'])
+def recom():
+
+    y = []
+    user_input = request.form.get('user_input')
+    ind = np.where(pt.index == user_input)[0][0]
+    lis = sorted(list(enumerate(sim[ind])), key=lambda x: x[1], reverse=True)[0:6]
+    for i in lis:
+        y.append(pt.index[i[0]])
+    df2=book[book["Book-Title"].isin(y)].drop_duplicates("Book-Title")
+    print(df2)
+    return render_template("recomedation.html",
+                           data=df2,
+                           book_nam=list(df2['Book-Title'].values),
+                           autho=list(df2['Book-Author'].values),
+                           imag=list(df2['Image-URL-M'].values),
+                           )
+if __name__ == '__main__':
+    app.run(debug=True)
